b8c533ced7f93c48e14d6bc20f2c2181263c7f13
Diego Moya <diego0697ms@gmail.com>
2025-06-05 21:36:05 +0200

    Feature: Code base

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,14 +30,21 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={20} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="library"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Library',
+          tabBarIcon: ({ color }) => <IconSymbol size={20} name="library" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <IconSymbol size={20} name="settings.fill" color={color} />,
         }}
       />
     </Tabs>
